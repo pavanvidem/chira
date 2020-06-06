26fa1933d3565ebeb6f21462751a27a0394248c4
Pavankumar Videm <pavanvidem@gmail.com>
2020-06-06 10:51:28 +0200

    update version number

--- chira_extract.py
+++ chira_extract.py
@@ -646,7 +646,7 @@ if __name__ == "__main__":
     parser.add_argument("-s", '--summerize', action='store_true', dest='summerize',
                         help="Summerize interactions at loci level")
 
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.3.2')
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.3.3')
 
     args = parser.parse_args()
 
